1998d71b6225b73b6009e4f28ad80578a012d7e3
小冰 <wupantong@wanxue.cn>
2022-02-08 12:05:43 +0800

    build: tailwindcss
    1: 添加单位转换配置

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="w-full h-full p-8 flex flex-col justify-center content-center">
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <div class="flex flex-col content-center justify-center w-full h-full p-8">
+    <figure class="p-8 md:flex bg-slate-100 rounded-xl md:p-0 dark:bg-slate-800">
       <img
-        class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+        class="w-24 h-24 mx-auto rounded-full md:w-48 md:h-auto md:rounded-none"
         src="/@/assets/images/logo.png"
         alt=""
         width="384"
         height="512"
       />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+      <div class="pt-6 space-y-4 text-center md:p-8 md:text-left">
         <blockquote>
           <p class="text-lg font-medium">
-            “Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to
-            customize, adapts to any design, and the build size is tiny.”
+            “Tailwind
+            CSS是我所见过的唯一一个可以在大型团队中进行扩展的框架。它很容易定制，适用于任何设计，而且构建尺寸很小。”
           </p>
         </blockquote>
         <figcaption class="font-medium">
-          <div class="text-sky-500 dark:text-sky-400"> Sarah Dayan </div>
-          <div class="text-slate-700 dark:text-slate-500"> Staff Engineer, Algolia </div>
+          <div class="text-sky-500 dark:text-sky-400"> 莎拉·达扬 </div>
+          <div class="text-slate-700 dark:text-slate-500"> 主管工程师,阿尔戈利亚 </div>
         </figcaption>
       </div>
     </figure>
