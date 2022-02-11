d2addd5839478f6226d2aabdce4f90d5064803c7
小冰 <wupantong@wanxue.cn>
2022-02-11 17:25:10 +0800

    build: 配置
    1: vscode
    2: tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        8: '28px',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
 };
